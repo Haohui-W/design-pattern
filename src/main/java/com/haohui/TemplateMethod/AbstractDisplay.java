873b20b42b04448c8e2702e67376a8c193bd5532
Haohui <haohui-w@qq.com>
2022-06-20 14:58:50 +0800

    模板方法

---
+++ src/main/java/com/haohui/TemplateMethod/AbstractDisplay.java
@@ -0,0 +1,19 @@
+package com.haohui.TemplateMethod;
+
+public abstract class AbstractDisplay {
+    /**
+     * 模板方法，定义了任务的处理逻辑，也就是个模板。
+     * 但是具体的操作，由子类决定
+     */
+    public final void display(){
+        open();
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+        close();
+    }
+
+    public abstract void open();
+    public abstract void print();
+    public abstract void close();
+}
---
+++ src/main/java/com/haohui/TemplateMethod/Main.java
@@ -0,0 +1,14 @@
+package com.haohui.TemplateMethod;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractDisplay display1 = new CharDisplay('我');
+        AbstractDisplay display2 = new StringDisplay("你好，世界！");
+        AbstractDisplay display3 = new StringDisplay("Hello World!");
+
+        display1.display();
+        display2.display();
+        display3.display();
+
+    }
+}
